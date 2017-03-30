510479b892bc1b6918e3137ec2f3ee398c74b216
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-03-30 16:34:37 -0700

    Add figure 3 maps with lakes

--- figure-3.R
+++ figure-3.R
@@ -23,7 +23,7 @@ vatal.data <- read.csv(file = "data/vanessa-atalanta-data.csv",
 ################################################################################
 # Plotting points on map, one map for each of three days
 long.bound <- c(-85, -60)
-lat.bound <- c(41, 48)
+lat.bound <- c(42, 47.5)
 
 dates <- c("2012-04-15", "2012-04-16", "2012-04-17")
 
@@ -33,7 +33,7 @@ par(mfrow = c(3, 1),
     mar = c(1, 2, 1, 1) + 0.1)
 for (one.date in dates) {
   to.plot <- vatal.data[vatal.data$date == one.date, ]
-  map(database = "worldHires",
+  map(database = "world",# "worldHires",
       xlim = long.bound,
       ylim = lat.bound,
       col = "#E7E7E7",
@@ -54,30 +54,109 @@ par(mfrow = c(1, 1),
 dev.off()
 
 
-map(database = "worldHires",
-    xlim = long.bound,
-    ylim = lat.bound,
-    col = "#E7E7E7",
-    fill = TRUE)
-points(x = vatal.data$longitude,
-       y = vatal.data$latitude,
-       cex = 1.2, 
-       pch = 21, 
-       bg = "red",
-       col = "black")
 
 
-map(database = "worldHires", 
-    xlim = c(-125, -115), 
-    ylim = c(31, 41), 
-    col = "#E7E7E7", 
-    fill = TRUE)
-points(x = localities$longitude, 
-       y = localities$latitude, 
-       cex = 1.2, 
-       pch = 21, 
-       bg = point.cols, 
-       col = "black")
+################################################################################
+# Plotting points on map, one map for each of three days
+#install.packages("maps")
+library(maps)
+#install.packages("sp")
+#install.packages("raster")
+library("raster")
+
+long.bound <- c(-85, -60)
+lat.bound <- c(41, 48) # lower limit must be < 43, or Lakes Erie & Ontario won't be drawn
+
+dates <- c("2012-04-15", "2012-04-16", "2012-04-17")
+
+us <- getData(name = "GADM", country = "USA", level = 1, path = "data/")
+canada <- getData(name = "GADM", country = "CAN", level = 1)
+
+# Subsetting data draws maps faster
+us.states <- c("Maine", "New Hampshire", "Vermont", "Massachusetts", "Rhode Island",
+               "Connecticut", "New York", "New Jersey", "Pennsylvania", "Ohio",
+               "Michigan", "Indiana", "Deleware", "Maryland", "West Virginia", 
+               "Virginia", "Kentucky", "District of Columbia")
+us.state.data <- us[us$NAME_1 %in% us.states, ]
+ca.provinces <- c("Ontario", "Québec", "New Brunswick", "Nova Scotia", 
+                  "Prince Edward Island")
+ca.province.data <- canada[canada$NAME_1 %in% ca.provinces, ]
+
+
+pdf(file = "output/figure-3-maps.pdf", useDingbats = FALSE)
+par(mfrow = c(3, 1),
+    mar = c(1, 2, 1, 1) + 0.1)
+for (one.date in dates) {
+  to.plot <- vatal.data[vatal.data$date == one.date, ]
+  # Create plot for desired data
+
+  # Use map to establish the image boundaries, as plot will *expand* the drawing area if 
+  # it is called first
+  map(database = "worldHires", 
+      xlim = long.bound, 
+      ylim = lat.bound)
+  # Draw US States
+  # plot(us.state.data, 
+  #      col = "#E7E7E7", 
+  #      add = TRUE)
+  map(us.state.data,
+      col = "#E7E7E7",
+      fill = TRUE,
+      add = TRUE)
+  # Add Canada provinces
+  # plot(ca.province.data, 
+  #      col = "#E7E7E7", 
+  #      add = TRUE)
+  map(ca.province.data,
+      col = "#E7E7E7",
+      fill = TRUE,
+      add = TRUE)
+  # Add lakes
+  map(database = "lakes", add = TRUE,
+      xlim = long.bound,
+      ylim = lat.bound,
+      col = "#FFFFFF",
+      fill = TRUE)
+  # Add points
+  points(x = to.plot$longitude,
+         y = to.plot$latitude,
+         cex = 1.2, 
+         pch = 21, 
+         bg = "orangered",
+         col = "black")
+  legend("bottomright", 
+         legend = one.date, 
+         cex = 2.0,
+         bty = "n")
+}
+par(mfrow = c(1, 1),
+    mar = c(5, 4, 4, 2) + 0.1)
+dev.off()
+
+##########
+
+par(mfrow = c(3, 1),
+    mar = c(1, 2, 1, 1) + 0.1)
+for (one.date in dates) {
+  to.plot <- vatal.data[vatal.data$date == one.date, ]
+  map(database = "world",# "worldHires",
+      xlim = long.bound,
+      ylim = lat.bound,
+      col = "#E7E7E7",
+      fill = TRUE)
+  points(x = to.plot$longitude,
+         y = to.plot$latitude,
+         cex = 1.2, 
+         pch = 21, 
+         bg = "orangered",
+         col = "black")
+  legend("bottomright", 
+         legend = one.date, 
+         cex = 2.0,
+         bty = "n")
+}
+par(mfrow = c(1, 1),
+    mar = c(5, 4, 4, 2) + 0.1)
 
 
 
