1b3a3291d5dd8cfe4437d01e514df97ecf2f1524
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-03-31 15:30:45 -0700

    Figure 2 idea for code

---
+++ figure-2.R
@@ -0,0 +1,10 @@
+# Mapping observation/checklist density
+# Jeffrey C. Oliver
+# jcoliver@email.arizona.edu
+# 2017-03-31
+
+rm(list = ls())
+
+################################################################################
+# See: https://visualecology.wordpress.com/2014/09/29/displaying-plots-or-points-by-density-in-r-with-the-ggmap-package/
+
