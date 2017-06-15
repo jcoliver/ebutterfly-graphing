baf2d7e711195a24b328aff7d24518683ada42e0
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-06-14 18:33:00 -0700

    Cleanup figure 4 script

--- figure-4.R
+++ figure-4.R
@@ -54,7 +54,7 @@ ca.provinces <- c("Ontario", "Québec", "New Brunswick", "Nova Scotia",
                   "Prince Edward Island")
 ca.province.data <- canada[canada$NAME_1 %in% ca.provinces, ]
 
-pdf(file = "output/figure-4-raster.pdf", useDingbats = FALSE)
+pdf(file = "output/figure-4.pdf", useDingbats = FALSE)
 par(mfrow = c(3, 1),
     mar = c(1, 2, 1, 1) + 0.1)
 for (one.month in incl.months) {
